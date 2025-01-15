c427410a330e2d2986858881571408b66f84c108
brian7346 <doctorbrian97@gmail.com>
2025-01-15 23:39:45 +0400

    dir fix

--- cmd.sh
+++ cmd.sh
@@ -3,6 +3,12 @@ set -e
 
 if [ "$ENV" = 'UNIT' ]; then
     echo "Running Unit Tests"
+    echo "Current directory: $(pwd)"
+    echo "Contents of current directory:"
+    ls -la
+    echo "Contents of /app:"
+    ls -la /app
+    cd /app  # Переходим в директорию с тестами
     exec python "tests.py"
 elif [ "$ENV" = 'DEV' ]; then
     echo "Running Development Server"
